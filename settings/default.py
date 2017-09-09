2d690319da51e142deffa189b22c322d234c4fbe
JKatzwinkel <JKatzwinkel@users.noreply.github.com>
2017-09-09 05:49:08 +0200

    install redis queue

--- settings/default.py
+++ settings/default.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = (
     'accounts',
     'endpoint',
     'annotationsets',
+    'django_rq',
 )
 
 AUTH_USER_MODEL = 'accounts.User'
@@ -183,6 +184,26 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     )
+}
+
+
+RQ_QUEUES = {
+    'default': {
+        'HOST': 'localhost',
+        'PORT': 6379,
+        'DB': 0,
+        'PASSWORD': 'some-password',
+        'DEFAULT_TIMEOUT': 360,
+    },
+    'high': {
+        'URL': os.getenv('REDISTOGO_URL', 'redis://localhost:6379/0'), # If you're on Heroku
+        'DEFAULT_TIMEOUT': 500,
+    },
+    'low': {
+        'HOST': 'localhost',
+        'PORT': 6379,
+        'DB': 0,
+    }
 }
 
 # neonion specific settings
